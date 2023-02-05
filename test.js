4c7fc423cdd852801589e55de096a42969b5e675
Степан Картавых <sktvpx@gmail.com>
2023-02-05 14:01:10 +0300

    red

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-a = 1
+const xArray = [50,60,70,80,90,100,110,120,130,140,150];
+const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Plot Scatter
+ctx.fillStyle = "red";
+for (let i = 0; i < xArray.length-1; i++) {
+  let x = xArray[i]*400/150;
+  let y = yArray[i]*400/15;
+  ctx.beginPath();
+  ctx.ellipse(x, y, 2, 3, 0, 0, Math.PI * 2);
+  ctx.fill();
+}
